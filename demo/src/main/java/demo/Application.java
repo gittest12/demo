ee0b651815d8e478ff8084629d4eb8e782bf870c
Shachar Ochyon <shachar@wave-2-wave.com>
2014-09-10 18:29:16 +0300

    Changed Application

--- demo/src/main/java/demo/Application.java
+++ demo/src/main/java/demo/Application.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
+    	System.out.println("Test...");
         SpringApplication.run(Application.class, args);
     }
 }
