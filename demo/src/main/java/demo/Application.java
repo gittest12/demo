7f949c523798f3471b90d778d47bd462cc49352e
Shachar Ochyon <shachar@wave-2-wave.com>
2014-09-10 18:39:44 +0300

    Changing master branch

--- demo/src/main/java/demo/Application.java
+++ demo/src/main/java/demo/Application.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-    	System.out.println("Test...");
+    	System.out.println("Test... Master branch");
         SpringApplication.run(Application.class, args);
     }
 }
