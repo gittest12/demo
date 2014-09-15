0fd04123abfcc9f051ac378f74aa1433305e538b
mica <mica@wave-2-wave.com>
2014-09-15 10:36:51 +0300

    Changing test message

--- demo/src/main/java/demo/Application.java
+++ demo/src/main/java/demo/Application.java
@@ -21,7 +21,7 @@ public class Application {
     private static ServiceRegistry serviceRegistry;
     
     public static void main(String[] args) {
-    	System.out.println("Test... develop branch");
+    	System.out.println("Test....");
         SpringApplication.run(Application.class, args);
         
 
